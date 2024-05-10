872a3aefe40c7987e66bc4e9628746ab53afc5b8
dhiv <vetri93selvi@gmail.com>
2024-05-10 16:24:40 +0530

    task solution

---
+++ T1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int a=1,b=2,c=3,d=4;
+        {
+            if((a+b)>(c+d));
+        }
+        System.out.println("a+b is greater than c+d");
+    }
+}
