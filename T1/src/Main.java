6d9b2256ac751405929d71e4dc063bd08d0cfcb9
sandhiv93 <167868273+sandhiv93@users.noreply.github.com>
2024-06-04 14:37:04 +0530

    Delete T1/src directory

--- T1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        int a=1,b=2,c=3,d=4;
-        {
-            if((a+b)>(c+d));
-        }
-        System.out.println("a+b is greater than c+d");
-    }
-}
